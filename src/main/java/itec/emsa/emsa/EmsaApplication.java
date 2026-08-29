258fbb72cd35243d4fb6b11d6db5072cdf522498
NahuelA <76659188+NahuelA@users.noreply.github.com>
2026-08-28 21:06:30 -0300

    feat: intial config

---
+++ src/main/java/itec/emsa/emsa/EmsaApplication.java
@@ -0,0 +1,13 @@
+package itec.emsa.emsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itec/emsa/emsa/EmsaApplicationTests.java
@@ -0,0 +1,13 @@
+package itec.emsa.emsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
