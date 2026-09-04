d891eb75d6a267c36d78f3fb049c4bf42dc7387c
pablo gargano <mailpablogargano@gmail.com>
2026-09-04 19:16:08 -0300

    atributo clase cliente

--- emsa/src/main/java/ar/edu/itec1/Cliente.java
+++ emsa/src/main/java/ar/edu/itec1/Cliente.java
@@ -1,4 +1,20 @@
 package ar.edu.itec1;
 
 public class Cliente {
+    private String nombre;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
